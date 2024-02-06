093271e09222c439c2d6b3a434ea3f2c2893b4e2
Ismail <hummetzadeismail@gmail.com>
2024-02-07 01:21:43 +0300

    text

--- app/_Components/Post.tsx
+++ app/_Components/Post.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Post = ({ text, title }: { text: string; title: string }) => {
   return (
-    <div className='flex gap-4 flex-col height-[500px] w-[500px]'>
+    <div className='flex gap-5 flex-col height-[500px] w-[500px]'>
       <div>Автор-Аноним</div>
-      <div>Название-{text}</div>
+      <div>Название-{title}</div>
       <p>{text}</p>
     </div>
   );
--- app/pyhics/page.tsx
+++ app/pyhics/page.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Post from '../_Components/Post';
-import { text4 } from '../_Texts/programmist';
+import { text4, text5 } from '../_Texts/programmist';
 
 const Page = () => {
   return (
     <div>
       <div className='w-full h-full flex flex-col gap-5 justify-center items-center'>
         <Post text={text4} title='Предоставление общей информации о физике' />
+        <Post
+          text={text5}
+          title='Первым этапом веб-разработки является планирование и анализ требований.'
+        />
       </div>
     </div>
   );
