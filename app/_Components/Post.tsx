3f7fd673dde15b40cb2410e2a67e36e5b3576b8e
Ismail <hummetzadeismail@gmail.com>
2024-02-07 01:28:29 +0300

    text

--- app/_Components/Post.tsx
+++ app/_Components/Post.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Post = ({ text, title }: { text: string; title: string }) => {
   return (
-    <div className='flex gap-5 flex-col height-[500px] w-[500px]'>
-      <div>Автор-Аноним</div>
-      <div>Название-{title}</div>
+    <div className='flex gap-5 flex-col height-[500px] w-[500px] border-solid border-zinc-100 border-4'>
+      <div className='border-zinc-100 border-2'>Автор-Аноним</div>
+      <div className='border-zinc-100 border-2'>Название-{title}</div>
       <p>{text}</p>
     </div>
   );
